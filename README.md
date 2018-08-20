0b8e1715bd50823e567ec310faa3d287bc960244
Jason Hicks <jaszhix@gmail.com>
2018-08-19 20:40:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nv-fan-tweak
+
+This is a simple Node script that is intended to run in the background and control the fan speed based on a curve. The fan curve and interval is configured by editing config.json.
+
+# Usage
+
+```sh
+npm pack .
+npm install -g nv-fan-tweak-1.0.0.tgz
+nv-fan-tweak
+```
---
+++ index.mjs
@@ -0,0 +1,78 @@
+import {exec} from 'child_process';
+import pkgJson from './package.json';
+import config from './config.json';
+
+process.title = pkgJson.name;
+
+const {fanCurve, interval} = config;
+let timeout;
+
+const convertRange = (value, r1, r2) => {
+  return (value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0];
+};
+
+const exc = (cmd) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(stdout.trim());
+    });
+  });
+};
+
+export const getFanSpeed = (tempString) => {
+  let temp = tempString.match(/(\d)+/g);
+  if (!temp) throw new Error('Didn\'t receive a fan speed value from nvidia-smi.');
+  temp = parseInt(temp[0]);
+
+  let range;
+  let len = fanCurve.length
+  let firstNotch = fanCurve[0];
+  let lastNotch = fanCurve[len - 1];
+
+  for (let i = 0; i < len; i++) {
+    let notch = fanCurve[i];
+    let prevNotch = fanCurve[i - 1];
+    let nextNotch = fanCurve[i + 1];
+
+    if (!prevNotch) prevNotch = Object.assign({}, {end: true}, fanCurve[0]);
+    if (!nextNotch) nextNotch = Object.assign({}, {end: true}, fanCurve[len - 1]);
+
+    if (temp <= firstNotch.temp) return firstNotch.fanSpeed;
+    if (temp >= lastNotch.temp) return lastNotch.fanSpeed;
+    if (temp <= notch.temp && (temp >= prevNotch.temp)) {
+      range = [[prevNotch.temp, notch.temp], [prevNotch.fanSpeed, notch.fanSpeed]];
+    } else if (temp > notch.temp && (temp <= nextNotch.temp)) {
+      range = [[notch.temp, nextNotch.temp], [notch.fanSpeed, nextNotch.fanSpeed]];
+    }
+    if (range) break;
+  }
+
+  let fanSpeed = convertRange(temp, ...range);
+  if (fanSpeed < 0) {
+    fanSpeed = 0;
+  } else if (fanSpeed > 100) {
+    fanSpeed = 100;
+  }
+  return fanSpeed;
+};
+
+const setFanSpeed = (fanSpeed) => {
+  return exc(`nvidia-settings -a [fan:0]/GPUTargetFanSpeed=${fanSpeed}`);
+};
+
+const checkTemp = () => {
+  exc('nvidia-smi -q --gpu=1 | grep "GPU Current Temp"')
+    .then(getFanSpeed)
+    .then(setFanSpeed)
+    .then(() => {
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(checkTemp, interval);
+    })
+    .catch((err) => console.log(err));
+};
+
+if (!process.env.NVFT_TEST) checkTemp();
