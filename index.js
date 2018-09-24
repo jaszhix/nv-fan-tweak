0e6b7918053862ba13027ae76bdd48450b6c0ee3
Jason Hicks <jaszhix@gmail.com>
2018-09-24 11:13:21 -0500

    Only get fan speeds from active GPUs
    The GPU index and fan index can be different when multiple GPUs are
    present, but not all are active.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const {rEach} = require('./lang.js');
 
 process.title = pkgJson.name;
 
-let timeout;
+let timeout, GPUs = [];
 
 const convertRange = (value, r1, r2) => {
   return (value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0];
 };
 
 const getFanSpeed = (args) => {
-  let [tempString, gpuIndex] = args;
-  let fanCurve = fanCurves[gpuIndex];
+  let [tempString, fanIndex] = args;
+  let fanCurve = fanCurves[fanIndex];
   if (!fanCurve) throw new Error('getFanSpeed: Invalid GPU index.');
   let temp = tempString.match(/(\d)+/g);
   if (!temp) throw new Error('Didn\'t receive a temperature value from nvidia-smi.');
@@ -32,8 +32,8 @@ const getFanSpeed = (args) => {
     if (!prevNotch) prevNotch = Object.assign({}, {end: true}, fanCurve[0]);
     if (!nextNotch) nextNotch = Object.assign({}, {end: true}, fanCurve[len - 1]);
 
-    if (temp <= firstNotch.temp) return firstNotch.fanSpeed;
-    if (temp >= lastNotch.temp) return lastNotch.fanSpeed;
+    if (temp <= firstNotch.temp) return [firstNotch.fanSpeed, fanIndex];
+    if (temp >= lastNotch.temp) return [lastNotch.fanSpeed, fanIndex];
     if (temp <= notch.temp && (temp >= prevNotch.temp)) {
       range = [[prevNotch.temp, notch.temp], [prevNotch.fanSpeed, notch.fanSpeed]];
     } else if (temp > notch.temp && (temp <= nextNotch.temp)) {
@@ -48,24 +48,25 @@ const getFanSpeed = (args) => {
   } else if (fanSpeed > 100) {
     fanSpeed = 100;
   }
-  return fanSpeed;
+  return [fanSpeed, fanIndex];
 };
 
-const setFanSpeed = (fanSpeed) => {
-  return exc(`nvidia-settings -a [fan:0]/GPUTargetFanSpeed=${fanSpeed}`);
+const setFanSpeed = (args) => {
+  let [fanSpeed, fanIndex] = args;
+  return exc(`nvidia-settings -a [fan:${fanIndex}]/GPUTargetFanSpeed=${fanSpeed}`);
 };
 
-const getTemp = (gpuIndex) => {
+const getTemp = (fanIndex, gpuIndex) => {
   return new Promise((resolve, reject) => {
     exc(`nvidia-smi -q --gpu=${gpuIndex} | grep "GPU Current Temp"`).then((tempString) => {
-      resolve([tempString, gpuIndex]);
+      resolve([tempString, fanIndex]);
     }).catch((err) => reject(err))
   });
 };
 
 const checkTemp = () => {
   rEach(fanCurves, (fanCurve, i, next) => {
-    getTemp(i)
+    getTemp(i, fanCurve.gpuIndex)
       .then(getFanSpeed)
       .then(setFanSpeed)
       .then(next)
@@ -76,6 +77,44 @@ const checkTemp = () => {
   });
 };
 
-if (!process.env.NVFT_TEST) checkTemp();
+const parseSMI = (gpuIndex) => {
+  return new Promise((resolve, reject) => {
+    exc(`nvidia-smi -q --gpu=${gpuIndex}`).then((tempString) => {
+      let lines = tempString.split('\n').filter((line) => line.includes(':'));
+      for (let i = 0; i < lines.length; i++) {
+        let [key, value] = lines[i].split(':');
+        if (key.trim() === 'Display Mode' && value.trim() === 'Disabled') {
+          return reject(`GPU ${gpuIndex} is disabled.`);
+        }
+      }
+      resolve(gpuIndex);
+    }).catch((err) => reject(err))
+  });
+};
+
+const init = () => {
+  rEach(fanCurves, (fanCurve, i, next) => {
+    parseSMI(i)
+      .then((gpuIndex) => GPUs.push(gpuIndex))
+      .then(next)
+      .catch((err) => {
+        console.log(err);
+        next();
+      });
+  }, () => {
+    while (fanCurves.length < GPUs.length) {
+      fanCurves.push(fanCurves[fanCurves.length - 1]);
+    }
+    while (fanCurves.length > GPUs.length) {
+      fanCurves.splice(-1);
+    }
+    for (let i = 0; i < fanCurves.length; i++) {
+      fanCurves[i].gpuIndex = GPUs[i];
+    }
+    if (!process.env.NVFT_TEST) checkTemp();
+  });
+};
+
+init();
 
 module.exports = {getFanSpeed};
